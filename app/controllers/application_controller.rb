2b0c60cbe7b06a2a8aa75537cdded496f1069c6b
tadat017 <tadat017@gmail.com>
2024-07-15 09:45:00 +0900

    ユーザー管理機能１．２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth])
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'items/index'
+  devise_for :users,controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'orders#index'
+  root to: 'items#index'#トップページへ
   resources :orders, only:[:create]
   # Defines the root path route ("/")
   # root "articles#index"
