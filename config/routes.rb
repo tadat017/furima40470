8c95cc76cfacc231e9cb942a74eb15fb27cd1d7c
tadat017 <tadat017@gmail.com>
2024-07-27 00:11:25 +0900

    r

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items, only: [:new, :create, :index]
+  resources :items
   resources :orders, only:[:create]
   # Defines the root path route ("/")
   # root "articles#index"
