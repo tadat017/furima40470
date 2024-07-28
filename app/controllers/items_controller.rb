99007019fe2148b5e5f5f3d5bc29f8f4f6b31db9
tadat017 <tadat017@gmail.com>
2024-07-28 18:24:40 +0900

    2.3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update,:show]
   before_action :move_to_root, only: [:edit, :update]
 
   def index
@@ -33,7 +33,7 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
+    
   end
      
   private
