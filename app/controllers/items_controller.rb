d978dfe536b1e46a7a17a48756f130f4f01aae74
tadat017 <tadat017@gmail.com>
2024-07-28 13:38:46 +0900

    編集機能1.0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit ]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -7,6 +7,24 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
+  end
+
+ def update
+  @item = Item.find(params[:id])
+  if current_user.id != @item.user_id
+    redirect_to root_path
+  elsif @item.update(item_params)
+    redirect_to item_path(@item)
+   else
+    render :edit,status: :unprocessable_entity
+   end
   end
 
   def create
