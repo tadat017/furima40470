b0634b36e8e0c6bea48f16dab9776fe617aa457f
tadat017 <tadat017@gmail.com>
2024-07-28 17:02:58 +0900

    編集2.0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :move_to_root, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -10,17 +12,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if current_user.id != @item.user_id
-      redirect_to root_path
-    end
-  end
+     end
 
  def update
-  @item = Item.find(params[:id])
-  if current_user.id != @item.user_id
-    redirect_to root_path
-  elsif @item.update(item_params)
+  
+  if @item.update(item_params)
     redirect_to item_path(@item)
    else
     render :edit,status: :unprocessable_entity
@@ -45,4 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price,  :image).merge(user_id: current_user.id)
   end
+end
+
+def set_item
+  @item = Item.find(params[:id])
+end
+
+def move_to_root
+  redirect_to root_path if current_user.id != @item.user_id
 end
