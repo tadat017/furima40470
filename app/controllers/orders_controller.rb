32624e4e503f21d9898c88b5a7602ad2f3b3a797
tadat017 <tadat017@gmail.com>
2024-08-05 07:41:22 +0900

    card.jsを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,22 +6,31 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
+
     if @order.valid?
+      process_payment
       @order.save
-      return redirect_to root_path
+      redirect_to root_path
     else
-      render 'index', status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
-
-
   private
 
-  def order_params
-    params.require(:order).permit(:price)
+  def process_payment
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'
+    )
   end
 
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
 
 end
