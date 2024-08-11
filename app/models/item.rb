9b5f3df08005674c4615123861ce516a075be9d5
tadat017 <tadat017@gmail.com>
2024-08-11 19:48:01 +0900

    日数

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
   validates :name, :description, :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image, presence: true
-  validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 0, message: "を選択してください" }
+  validates :category_id, :condition_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :price, numericality: { 
     greater_than_or_equal_to: 300, 
     less_than_or_equal_to: 9_999_999, 
