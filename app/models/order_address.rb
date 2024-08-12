17ee2d411c6bcec6ca72be1146de192632ffd55b
tadat017 <tadat017@gmail.com>
2024-08-12 17:07:43 +0900

    都道府県バリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' } 
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' } 
     validates :city
     validates :addresses
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter it as a 10 or 11 digit number' }
